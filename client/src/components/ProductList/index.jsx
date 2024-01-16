52e0e0eab4011ebe3fa5f47f0e3263f1e3c6f6ee
WebDevMullins <webdevmullins@gmail.com>
2024-01-16 12:17:18 -0600

    refactor ProductList component to use Redux

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -1,70 +1,76 @@
-import { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+import { useQuery } from '@apollo/client'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import spinner from '../../assets/spinner.gif'
+import { idbPromise } from '../../utils/helpers'
+import { QUERY_PRODUCTS } from '../../utils/queries'
+import ProductItem from '../ProductItem'
+
+import { updateProducts } from '../../redux/productsSlice'
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+	// Initialize useDispatch and useSelector hooks from React Redux
+	const dispatch = useDispatch()
+	const { currentCategory, products } = useSelector((state) => state)
 
-  const { currentCategory } = state;
+	// Use Apollo useQuery hook to fetch product data
+	const { loading, data } = useQuery(QUERY_PRODUCTS)
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+	// useEffect hook to handle updates when data or loading changes
+	useEffect(() => {
+		// Check if data is available
+		if (data) {
+			// Dispatch the updateProducts action to update Redux store
+			dispatch(updateProducts(data.products))
 
-  useEffect(() => {
-    if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
-      });
-    }
-  }, [data, loading, dispatch]);
+			// Update IndexedDB with the new products data
+			data.products.forEach((product) => {
+				idbPromise('products', 'put', product)
+			})
+		} else if (!loading) {
+			// If data is not available and not loading, retrieve data from IndexedDB
+			idbPromise('products', 'get').then((products) => {
+				dispatch(updateProducts(products))
+			})
+		}
+	}, [data, loading, dispatch])
 
-  function filterProducts() {
-    if (!currentCategory) {
-      return state.products;
-    }
+	// Function to filter products based on the current category
+	function filterProducts() {
+		if (!currentCategory) {
+			return products
+		}
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
-  }
+		return products.filter((product) => product.category._id === currentCategory)
+	}
 
-  return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
-      {state.products.length ? (
-        <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
-        </div>
-      ) : (
-        <h3>You haven't added any products yet!</h3>
-      )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </div>
-  );
+	return (
+		<div className='my-2'>
+			<h2>Our Products:</h2>
+			{/* Check if products are available */}
+			{products.length ? (
+				<div className='flex-row'>
+					{/* Map through filtered products and render ProductItem for each product */}
+					{filterProducts().map((product) => (
+						<ProductItem
+							key={product._id}
+							_id={product._id}
+							image={product.image}
+							name={product.name}
+							price={product.price}
+							quantity={product.quantity}
+						/>
+					))}
+				</div>
+			) : (
+				/* Display message if no products are available */
+				<h3>You haven&apos;t added any products yet!</h3>
+			)}
+			{/* Display spinner if data is still loading */}
+			{loading ? <img src={spinner} alt='loading' /> : null}
+		</div>
+	)
 }
 
-export default ProductList;
+export default ProductList
